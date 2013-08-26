0126a7e34e4c7319ead32b2370af2f889383ecfc
Jia De <cheokjiade@gmail.com>
2013-08-26 23:40:05 +0800

    added activity recognition

--- SensingClient/src/com/jiade/fyp/sensingclient/entities/SCall.java
+++ SensingClient/src/com/jiade/fyp/sensingclient/entities/SCall.java
@@ -1,5 +1,34 @@
 package com.jiade.fyp.sensingclient.entities;
 
 public class SCall {
-
+	public static final int INCOMMING = 0;
+	public static final int OUTOGOING = 1;
+	private int callIo;
+	private String callNo;
+	private long callDuration;
+	public SCall(int callIo, String callNo, long callDuration) {
+		super();
+		this.callIo = callIo;
+		this.callNo = callNo;
+		this.callDuration = callDuration;
+	}
+	public int getCallIo() {
+		return callIo;
+	}
+	public void setCallIo(int callIo) {
+		this.callIo = callIo;
+	}
+	public String getCallNo() {
+		return callNo;
+	}
+	public void setCallNo(String callNo) {
+		this.callNo = callNo;
+	}
+	public long getCallDuration() {
+		return callDuration;
+	}
+	public void setCallDuration(long callDuration) {
+		this.callDuration = callDuration;
+	}
+	
 }
---
+++ SensingClient/src/com/jiade/fyp/sensingclient/services/ActivityRecognitionService.java
@@ -0,0 +1,97 @@
+package com.jiade.fyp.sensingclient.services;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.ActivityRecognitionClient;
+
+import android.app.Dialog;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ActivityRecognitionService extends IntentService implements ConnectionCallbacks, OnConnectionFailedListener{
+
+	// Stores the PendingIntent used to send activity recognition events back to the app
+    private PendingIntent mActivityRecognitionPendingIntent;
+
+    // Stores the current instantiation of the activity recognition client
+    private ActivityRecognitionClient mActivityRecognitionClient;
+	public ActivityRecognitionService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+	private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Activity Recognition",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Get the error dialog from Google Play services
+            
+            return false;
+        }
+    }
+
+	@Override
+	public void onCreate() {
+		/*
+         * Instantiate a new activity recognition client. Since the
+         * parent Activity implements the connection listener and
+         * connection failure listener, the constructor uses "this"
+         * to specify the values of those parameters.
+         */
+        mActivityRecognitionClient =
+                new ActivityRecognitionClient(this, this, this);
+        /*
+         * Create the PendingIntent that Location Services uses
+         * to send activity recognition updates back to this app.
+         */
+        Intent intent = new Intent(
+                this, ActivityRecognitionService.class);
+        /*
+         * Return a PendingIntent that starts the IntentService.
+         */
+        mActivityRecognitionPendingIntent =
+                PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+		super.onCreate();
+	}
+}
