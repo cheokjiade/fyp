415093e97ac8e96f0adbc970e85163328d1ed279
Jia De <cheokjiade@gmail.com>
2014-05-08 23:34:10 +0800

    updated time variant to have the completed model. left with visualization

--- location/rawdata/timevariant.php
+++ location/rawdata/timevariant.php
@@ -28,18 +28,52 @@ $query->execute();
 $uniqueDates = $query->fetchAll(PDO::FETCH_ASSOC);
 $model = array("Monday"=>[],"Tuesday"=>[],"Wednesday"=>[],"Thursday"=>[],"Friday"=>[],"Saturday"=>[],"Sunday"=>[]);
 
-foreach($uniqueDates as $uniqueDate){
+$date = $uniqueDates[0]['udate'];
+$endDate = $uniqueDates[count($uniqueDates)-1]['udate'];
+while (strtotime($date) <= strtotime($endDate)) {
     for($i=0;$i<24;$i+=1){
-        if($i<10) $time = $uniqueDate['udate'] ." 0" . $i . ":00:00";
-        else $time = $uniqueDate['udate'] . " " . $i . ":00:00";
-       //SELECT * FROM stoppoint WHERE ( :time BETWEEN stoppoint_start_time AND stoppoint_end_time) AND session_hash = :session_hash
+        if($i<10) $time = $date ." 0" . $i . ":00:00";
+        else $time = $date . " " . $i . ":00:00";
+        //SELECT * FROM stoppoint WHERE ( :time BETWEEN stoppoint_start_time AND stoppoint_end_time) AND session_hash = :session_hash
         $query = $conn->prepare("SELECT * FROM stoppoint WHERE ( :time BETWEEN stoppoint_start_time AND stoppoint_end_time) AND session_hash = :session_hash;");
         $query->bindParam(":session_hash",$sessionHash);
         $query->bindParam(":time",$time);
         $query->execute();
         $stopPoint = $query->fetchAll(PDO::FETCH_ASSOC);
-        echo $time;
-        print_r($stopPoint);
+        if(!empty($stopPoint) && array_key_exists("locationpoint_id",$stopPoint[0])){
+            $dayName = date('l', strtotime($date));
+            if(!array_key_exists($i,$model[$dayName])){
+                $model[$dayName][$i] = array(/*"dates"=>array()*/);
+            }
+            if(!array_key_exists($stopPoint[0]['locationpoint_id'],$model[$dayName][$i])){
+                $model[$dayName][$i][$stopPoint[0]['locationpoint_id']] = 1;
+            }else {
+                $model[$dayName][$i][$stopPoint[0]['locationpoint_id']] = $model[$dayName][$i][$stopPoint[0]['locationpoint_id']] +1;
+            }
+            arsort($model[$dayName][$i]);
+            //$model[$dayName][$i]['dates'][]=$date;
+
+        }
+        //echo $time;
+
     }
+    $date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
 }
+print_r($model);
+
+
+//foreach($uniqueDates as $uniqueDate){
+//    for($i=0;$i<24;$i+=1){
+//        if($i<10) $time = $uniqueDate['udate'] ." 0" . $i . ":00:00";
+//        else $time = $uniqueDate['udate'] . " " . $i . ":00:00";
+//       //SELECT * FROM stoppoint WHERE ( :time BETWEEN stoppoint_start_time AND stoppoint_end_time) AND session_hash = :session_hash
+//        $query = $conn->prepare("SELECT * FROM stoppoint WHERE ( :time BETWEEN stoppoint_start_time AND stoppoint_end_time) AND session_hash = :session_hash;");
+//        $query->bindParam(":session_hash",$sessionHash);
+//        $query->bindParam(":time",$time);
+//        $query->execute();
+//        $stopPoint = $query->fetchAll(PDO::FETCH_ASSOC);
+//        echo $time;
+//        print_r($stopPoint);
+//    }
+//}
 ?>
